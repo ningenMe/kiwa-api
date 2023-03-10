ee486bdac25b9c38ffc3b318e5f61e6d1d001b3e
ningenMe <taishi.furuta@linecorp.com>
2023-03-11 01:03:50 +0900

    [add] first commit

---
+++ src/main/kotlin/ningenme/net/kiwaapi/KiwaApiApplication.kt
@@ -0,0 +1,11 @@
+package ningenme.net.kiwaapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KiwaApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KiwaApiApplication>(*args)
+}
---
+++ src/test/kotlin/ningenme/net/kiwaapi/KiwaApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package ningenme.net.kiwaapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KiwaApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

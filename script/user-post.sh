f0826490e3a10e2087304ac8853c7c957a0a01f2
ningenMe <taishi.furuta@linecorp.com>
2023-03-20 21:14:53 +0900

    [fix]

---
+++ script/user-post.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: application/json" -d '{"id":"user1","password":"Passw0rd"}' "localhost:8080/users"
--- src/main/kotlin/ningenme/net/kiwaapi/application/SecurityConfig.kt
+++ src/main/kotlin/ningenme/net/kiwaapi/application/SecurityConfig.kt
@@ -28,10 +28,6 @@ class SecurityConfig {
             .anyRequest()
             .authenticated()
 
-            .and()
-            .csrf()
-            .disable()
-
         return httpSecurity.build()
     }
 
